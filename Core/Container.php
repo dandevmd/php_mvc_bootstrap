f9a2aeebc373d3cd36e96f85a080bc07a08e2e47
dandevmd <danmunteanu420@gmail.com>
2023-12-01 14:59:50 +0200

    changed resolve from Container and added getClass

--- Core/Container.php
+++ Core/Container.php
@@ -13,16 +13,45 @@ class Container
   }
   public function resolve(string $key)
   {
-    if (!$this->has($key)) {
-      throw new \Exception("No resolver for $key");
+    if ($this->has($key)) {
+      $resolver = $this->bindings[$key];
+
+      return $resolver($this);
     }
 
-    $resolver = $this->bindings[$key];
-
-    return $resolver($this);
+    return $this->getClass($key);
   }
 
+  public function getClass(string $key): callable
+  {
+    //Inspect the class that we are trying get from the container
+    $reflectionClass = new \ReflectionClass($key);
 
+    // check if is instantiable
+    if (!$reflectionClass->isInstantiable()) {
+      throw new \Exception('Class ' . $key . ' is not instantiable');
+    }
+
+    // check if has constructor and if has parameters
+    if (!$reflectionClass->getConstructor() || !$reflectionClass->getConstructor()->getParameters()) {
+      return new $key;
+    }
+
+    //if has constructor and has parameters which are class/classes then resolve them
+    $dependencies = array_map(function (\ReflectionParameter $parameter) {
+      $name = $parameter->getName();
+      $type = $parameter->getType();
+
+      if ($type instanceof \ReflectionNamedType && !$type->isBuiltin()) {
+
+        return $this->resolve($type->getName());
+      }
+
+
+    }, $reflectionClass->getConstructor()->getParameters());
+
+    return $reflectionClass->newInstanceArgs($dependencies);
+  }
   public function has(string $key): bool
   {
     return isset($this->bindings[$key]);
